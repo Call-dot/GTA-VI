dda31d9e6ca4e77b8bc67e11c9939252e8dc9785
gnofnediaalt@gmail.com <gnofnedia@gmail.com>
2026-05-23 15:59:48 -0400

    Added acceleration and deceleration physics

--- entities/tile.py
+++ entities/tile.py
@@ -1,14 +1,27 @@
-# entities/tile.py
+#UNUSED, example syntax for later
 import pygame
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, game, image, x, y):
+        #Adds the tile to game.all_sprites
         super().__init__(game.all_sprites)
 
         self.image = image
         self.rect = self.image.get_rect(topleft=(x, y))
 
-        self.speed = 200  # pixels per second downward
+        self.speed = 20  # pixels per second downward
 
     def update(self, dt):
         self.rect.y += self.speed * dt
+
+# #In main.py:
+# from entities.tile import Tile
+# #self.road_tiles = []
+
+# road_img = self.assets.get_image("road_tile")
+# babyblue_car = self.assets.get_image("babyblue_car")
+
+# for y in range(-2, HEIGHT // TILESIZE + 2):
+#     for x in range(WIDTH // TILESIZE):
+#         tile = Tile(self, babyblue_car, x * TILESIZE, y * TILESIZE)
+#         self.road_tiles.append(tile)
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ WIDTH = 1068
 HEIGHT = 768
 X_CENTRE = WIDTH / 2
 Y_CENTRE = HEIGHT / 2
-TILESIZE = 1
+TILESIZE = 2
 
 class Game:
     def __init__(self):
@@ -19,21 +19,80 @@ class Game:
         self.running = True
         self.all_sprites = pygame.sprite.Group()
 
+        # Run AssetLoader and save it in self.assets
         self.assets = AssetLoader()
         self.assets.load_images()
+
+        # This variable controls whether the player is currently trying to accelerate
+        # Is directly updated by player
+        # Accelerating = 1
+        # Braking = 0
+        self.playermode = 0
+        # This variable is updated by another function when player presses down arrow
+        # Pixels per second downward
+        self.playerspeed = 0
+        # This variable controls whether the player is going:
+        # straight = 0
+        # left = 1
+        # right = 2
+        self.playerdir = 0
+
+        self.friction = -10 #px/s^2
+        self.max_speed = 300
+        
+        # Timestamps
+        self.ts_down = 0
 
     def run(self):
         while self.running:
             self.dt = self.clock.tick(30) / 1000
             self.events()
+            self.playerinput(self.dt)
             self.update()
-            self.bg()
+            self.bg_blitter()
             self.draw()
 
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+            if event.type == pygame.KEYDOWN:
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_DOWN]:
+                    self.ts_down = self.dt
+
+    def playerinput(self, dt):
+        keys = pygame.key.get_pressed()
+        
+        # Fast/slow
+        if keys[pygame.K_DOWN]:
+            self.accelerate()
+            self.debug("FORWARD!")
+        else:
+            self.decelerate()
+            # self.debug("SLOW DOWN!")
+
+        # Steering
+        if keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
+            self.playerdir = 1
+            self.debug("LEFT!")
+        elif keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]:
+            self.playerdir = 2
+            self.debug("RIGHT!")
+        else:
+            self.playerdir = 0
+
+    def accelerate(self):
+        acceleration = (self.max_speed - self.playerspeed) * 2
+        self.playerspeed += acceleration * self.dt
+        
+    def decelerate(self):
+        deceleration = (0 - self.playerspeed) * 2
+        print(self.playerspeed, "+=", deceleration, "*", self.dt)
+        self.playerspeed += deceleration * self.dt
+
+    def left_right(self, dt):
+        pass
 
     def update(self):
         self.all_sprites.update(self.dt)
@@ -43,20 +102,23 @@ class Game:
         self.all_sprites.draw(self.screen)        
         self.screen.blit(
             self.assets.get_image("road_tile"),
-            (X_CENTRE, Y_CENTRE)
+            (X_CENTRE + self.playerspeed, Y_CENTRE)
         )
-        print("drawing")
         pygame.display.flip()
 
-    def bg(self):
-        self.road_tiles = []
+    def bg_tiler(self):
+        """Generates a text file which represents the road that gets sent to bg_blitter"""
 
-        road_img = self.assets.get_image("road_tile")
+    def bg_blitter(self):
+        """Renders the text from bg_tiler into road images"""
+        
+    def debug(self, msg):
+        print(msg)
+        print("playermode:", self.playermode)
+        print("playerspeed:", self.playerspeed)
+        print("playerdir:", self.playerdir)
+        print("ts_down:", self.ts_down)
 
-        for y in range(-2, HEIGHT // TILESIZE + 2):
-            for x in range(WIDTH // TILESIZE):
-                tile = Tile(self, road_img, x * TILESIZE, y * TILESIZE)
-                self.road_tiles.append(tile)
 
 if __name__ == "__main__":
     pygame.init()
