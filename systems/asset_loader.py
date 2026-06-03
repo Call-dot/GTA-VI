02d98c622797483b9d9a199fedccc1acdf924a5d
Carey <Careyxintoronto@gmail.com>
2026-06-03 14:06:06 -0400

    added cars

--- systems/asset_loader.py
+++ systems/asset_loader.py
@@ -190,7 +190,15 @@ class AssetLoader:
         self.images["snowpine"] = pygame.image.load(
             image_dir / "snowpine.png"
         ).convert_alpha()
-        
+
+        self.images["policecar"] = pygame.image.load(
+            image_dir / "policecar.png"
+        ).convert_alpha()
+
+        self.images["policecar1"] = pygame.image.load(
+            image_dir / "policecar1.png"
+        ).convert_alpha()  
+
     def load_music(self):
         music_dir = Path("assets/music")
 
